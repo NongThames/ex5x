0c779ff68a5cd96f451ac516086d747aa541b247
Supawit <beet5346@gmail.com>
2022-09-24 17:33:17 +0700

    up

---
+++ src/ScoreWithEvent/Subscriber2.java
@@ -0,0 +1,8 @@
+package ScoreWithEvent;
+
+public class Subscriber2 implements ScoreListener {
+
+    public void scoreChange(ScoreEvent e) {
+        System.out.println("Live Result : " + e.getSomeData());
+   }
+}
